c4a7c42adc740e2acc288304b7c38a9062c84e42
Elivane <informatica.ecd@gmail.com>
2020-08-27 22:22:06 -0300

    Finalização do sistema

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function criarMusicaFactory(id, titulo) {
 
 function renderizaMusica(musica, indice) {
     return ` <div class="btn-video__wrapper">
-    <button type="button" class="btn-video__title">
+    <button type="button" class="btn-video__title" data-musica-codigo="${musica.id}">
         ${musica.titulo}
     </button>
     <button type="button" data-music-indice="${indice}" class="btn-video__remove" title="Remover vídeo">
@@ -32,6 +32,7 @@ function renderizaMusicas(p_musicas) {
         listaMusicas += renderizaMusica(p_musicas[i], i);
     }
     document.getElementById("musicWrapper").innerHTML = listaMusicas;
+    configPlayMusica();
     configDeleteMusica();
 
 
@@ -55,26 +56,38 @@ function configButtonClick() {
 
 }
 
-function removeMusicas() {
-    if (confirm("Deseja realmente remover a música?")) {
-        let indice = Number(this.getAttribute("data-music-indice"));
-        console.log(typeof indice, indice);
-        musicas.splice(indice, 1);
-        renderizaMusicas(musicas);
-    };
+function configPlayMusica() {
+    let btnPlay = document.querySelectorAll("[data-musica-codigo]");
+    btnPlay.forEach((obj) => {
+        obj.onclick = function () {
+            let codigoMusica = this.getAttribute("data-musica-codigo");
+            document.querySelector("[data-frame-musica]").src = `https://www.youtube.com/embed/e9VLnw17RJ8]`;
+        };
+    });
 }
-
 
 
 function configDeleteMusica() {
     let btnDelete = document.querySelectorAll("[data-music-indice]");
     console.log(btnDelete);
     btnDelete.forEach((obj) => {
-        obj.onclick = removeMusicas;
+        obj.onclick = function () {
+            if (confirm("Deseja realmente remover a música?")) {
+                let indice = Number(this.getAttribute("data-music-indice"));
+                console.log(typeof indice, indice);
+                musicas.splice(indice, 1);
+                renderizaMusicas(musicas);
+            }
+        };
 
     });
-
 }
+
+function abreFormCadastro() {
+    document.querySelector("[data-btn-add]").classList.remove("hidden");
+};
+
+
 
 function run() {
     musicas.push(criarMusicaFactory('ie9VLnw17RJ8', 'Título da primeira música'));
@@ -83,6 +96,31 @@ function run() {
     musicas.push(criarMusicaFactory('ie9VLnw17RJ8', 'Título da quarta música'));
     musicas.push(criarMusicaFactory('ie9VLnw17RJ8', 'Título da quinta música'));
     musicas.push(criarMusicaFactory('ie9VLnw17RJ8', 'Título da sexta música'));
+
+    document.querySelector("[data-btn-add]").onclick = function () {
+        this.classList.add("hidden");
+        document.querySelector("[data-form-add]").classList.remove("hidden");
+    };
+
+    document.querySelector("[data-form-add]").onsubmit = function (e) {
+        e.preventDefaul();
+        let txtTitulo = document.querySelector("#txtTitulo");
+        let txtCodigo = document.querySelector("#txtCodigo");
+
+        let musicaObj = criarMusicaFactory(txtCodigo.value, txtTitulo.value);
+
+        musicas.push(musicaObj);
+        renderizaMusicas(musicas);
+
+        this.reset();
+
+
+        document.querySelector('[data-form-add]').classList.add("hidden");  //this.classList.add('hidden');
+        document.querySelector('[data-btn-add]').classList.remove("hidden");
+
+
+    };
+
 
     renderizaMusicas(musicas);
     configButtonClick();
